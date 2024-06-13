21b58ff406bfdc5f3aa0c66180760d134a4cbfc3
Pedro Martins <pedro@pedromartins.com.pt>
2024-06-13 15:29:14 +0100

    Add Button TS component

---
+++ src/components/Button/Button.ts
@@ -0,0 +1,20 @@
+import styles from './Button.module.css'; 
+
+export class Button {
+  element: HTMLButtonElement;
+
+  constructor(label: string, type?: string) {
+    this.element = document.createElement('button');
+    this.element.textContent = label;
+
+    if (type && styles[type]) {
+      this.element.classList.add(styles.button, styles[type]);
+    } else {
+      this.element.classList.add(styles.button);
+    }
+  }
+
+  render() {
+    return this.element;
+  }
+}
