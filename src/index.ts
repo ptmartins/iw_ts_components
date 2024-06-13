c6ddee6105c291dfc09974d9790ec6a00ab3dea0
Pedro Martins <pedro@pedromartins.com.pt>
2024-06-13 15:41:58 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import { Button } from './components/Button/Button';
 
 const app = document.getElementById('app');
 
-if (app) {
-  const button1 = new Button('Button');
-  const button2 = new Button('Button Primary', 'primary');
-  const button3 = new Button('Button Kill', 'kill');
-  
-  app.append(button1.render(), button2.render(), button3.render());
-}
+
+const button1 = new Button('Button');
+const button2 = new Button('Button Primary', 'primary');
+const button3 = new Button('Button Kill', 'kill');
+
+app.append(button1.render(), button2.render(), button3.render());
