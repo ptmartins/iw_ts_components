e51a564e4405af408cfbc145a3adfe7769f5f57c
ptmartins <pedro@pedromartins.com.pt>
2024-06-14 09:48:13 +0100

    Add Card component.

---
+++ src/components/Card/Card.ts
@@ -0,0 +1,15 @@
+import styles from './Card.module.css';
+
+export class Card {
+    element: HTMLElement;
+  
+    constructor(title: string) {
+      this.element = document.createElement('DIV');
+      this.element.textContent = title;
+      this.element.className = styles.card;
+    }
+  
+    render() {
+      return this.element;
+    }
+  }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Button } from './components/Button/Button';
+import { Card } from './components/Card/Card';
 
 const app = document.getElementById('app');
 
@@ -7,4 +8,6 @@ const button1 = new Button('Button');
 const button2 = new Button('Button Primary', 'primary');
 const button3 = new Button('Button Kill', 'kill');
 
-app.append(button1.render(), button2.render(), button3.render());
+const card = new Card('Hello Card');
+
+app.append(button1.render(), button2.render(), button3.render(), card.render());
